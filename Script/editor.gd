e6fc4e16ace2682f11c99dd93264f4042cf5ca01
Oviglo <ovigne.loic@gmail.com>
2024-04-25 10:14:49 +0200

    fix editor drawing

--- Script/editor.gd
+++ Script/editor.gd
@@ -33,33 +33,20 @@ func _process(delta: float) -> void:
 		_cursor.position.x -= TILE_SIZE
 	if Input.is_action_just_pressed("move_right") && _cursor.position.x < ((MAP_WIDTH -1) * TILE_SIZE):
 		_cursor.position.x += TILE_SIZE
-		
-	var cursor_position: Vector2 = Vector2(_cursor.position.x / TILE_SIZE, _cursor.position.y / TILE_SIZE)
-	if Input.is_key_pressed(KEY_W):
-		create_tile(cursor_position, TYPE.WALL)
-	if Input.is_key_pressed(KEY_P):
-		create_tile(cursor_position, TYPE.PLAYER)
-	if Input.is_key_pressed(KEY_B):
-		create_tile(cursor_position, TYPE.BOX)
-	if Input.is_key_pressed(KEY_T):
-		create_tile(cursor_position, TYPE.TARGET)
-	if Input.is_key_pressed(KEY_E):
-		create_tile(cursor_position, TYPE.EMPTY)
-	if Input.is_key_pressed(KEY_S):
-		save_level(1)
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) || Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		var mouse_x: int = get_viewport().get_mouse_position().x
 		var mouse_y: int = get_viewport().get_mouse_position().y
 		
 		if (mouse_x < (MAP_WIDTH * TILE_SIZE) && mouse_y < (MAP_HEIGHT * TILE_SIZE)):
-			_cursor.position.x = floor(get_viewport().get_mouse_position().x / TILE_SIZE) * TILE_SIZE
-			_cursor.position.y = floor(get_viewport().get_mouse_position().y / TILE_SIZE) * TILE_SIZE
-			
-		if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-			create_tile(cursor_position, TYPE.EMPTY)			
-		elif Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-			create_tile(cursor_position, selected_tile)
+			_cursor.position.x = floor(mouse_x / TILE_SIZE) * TILE_SIZE
+			_cursor.position.y = floor(mouse_y / TILE_SIZE) * TILE_SIZE
+		
+			var cursor_position: Vector2 = Vector2(floor(mouse_x / TILE_SIZE), floor(mouse_y / TILE_SIZE))
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+				create_tile(cursor_position, TYPE.EMPTY)
+			elif Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				create_tile(cursor_position, selected_tile)
 
 func draw_selected_tile() -> void:
 	var texture: Texture2D = _tileMap.get_tileset().get_source(0).get_runtime_texture()
